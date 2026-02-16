8ac59e16af8ef1c156adfb4a1c1d726614c17966
sudhankandel <sudhankandel03@gmail.com>
2026-02-16 13:00:57 +1100

    Add addPerson() method skeleton with placeholder validation methods

--- SEFAssignment4/src/main/java/au/edu/rmit/sct/Person.java
+++ SEFAssignment4/src/main/java/au/edu/rmit/sct/Person.java
@@ -12,6 +12,7 @@ public class Person {
     private String lastName;
     private String address;
     private String birthdate;
+    private static final String FILE_NAME = "src/main/resources/persons.txt";
 
     public Person(String personID, String firstName, String lastName,
                   String address, String birthdate) {
@@ -20,13 +21,47 @@ public class Person {
         this.lastName = lastName;
         this.address = address;
         this.birthdate = birthdate;
+    
+
     }
 
+     private boolean isValidPersonID(String id) {
+
+        return true;
+     }
+      private boolean isValidAddress(String addr) {
+        return true;
+      }
+
+       private boolean isValidBirthdate(String bd) {
+        return true;
+       }
+        private boolean isDuplicateID(String id) {
+            return true;
+        }
 
     public boolean addPerson() {
 
-        return true;
+        if (!isValidPersonID(personID)) return false;
+
+        if (!isValidAddress(address)) return false;
+     
+        if (!isValidBirthdate(birthdate)) return false;
+
+        if (isDuplicateID(personID)) return false;
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(FILE_NAME, true))) {
+            writer.write(personID + "," + firstName + "," + lastName + "," + address + "," + birthdate);
+            writer.newLine();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
     }
+
+
 
 
     public boolean updatePersonalDetails() {
