da1a6a61613e6a361458048accea5e61878e3fbc
sudhankandel <sudhankandel03@gmail.com>
2026-02-16 15:04:21 +1100

    create skleton code for test functions

---
+++ SEFAssignment4/src/test/java/au/edu/rmit/sct/PersonTest.java
@@ -0,0 +1,35 @@
+package au.edu.rmit.sct;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonFunctionTests {
+
+    // Test cases for addPerson()
+    @Test
+    void testAddPerson_case1() {
+
+    }
+
+    @Test
+    void testAddPerson_case2() {
+
+    }
+
+    @Test
+    void testAddPerson_case3() {
+      
+    }
+
+    @Test
+    void testAddPerson_case4() {
+      
+    }
+
+    @Test
+    void testAddPerson_case5() {
+      
+    }
+
+
+}
